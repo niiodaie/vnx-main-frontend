3c444bb8e0c0cf6629ebc534dab8acf965bbe06a
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-19 23:21:23 -0500

    Update and rename tailwind.config.js js Copy Edit to tailwind.config.js

--- public/vnx-note/frontend/tailwind.config.js
+++ public/vnx-note/frontend/tailwind.config.js
@@ -1,5 +1,5 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
@@ -9,3 +9,4 @@ module.exports = {
   },
   plugins: [],
 };
+
