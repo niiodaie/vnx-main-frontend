5b509ab1b3b33f85d588acdce96b1b7651da4388
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-21 12:40:36 -0500

    Update main.tsx

--- vnx-shell/tools/vnx-notebook/src/main.tsx
+++ vnx-shell/tools/vnx-notebook/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Make sure Tailwind is configured in this file
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Tailwind entry, if used
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
