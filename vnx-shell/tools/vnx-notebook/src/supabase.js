4c0316b8c82b4405b202d3ba5205bf958f7053e5
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-22 05:49:32 -0500

    Update supabase.js

--- vnx-shell/tools/vnx-notebook/src/supabase.js
+++ vnx-shell/tools/vnx-notebook/src/supabase.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xuptevcpwsuxckuhsnwr.supabase.co';
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cHRldmNwd3N1eGNrdWhzbndyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDU1ODE2OSwiZXhwIjoyMDY2MTM0MTY5fQ.xUPuBYzsqUFlXO8hKjjMRiBsATe3TcV05aZ0D1o6kNk
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cHRldmNwd3N1eGNrdWhzbndyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NTgxNjksImV4cCI6MjA2NjEzNDE2OX0.eVNn5_PlNDOYD59OQf0EW0rs6X0QRPy5aQwnE7MGaF8
 export const supabase = createClient(supabaseUrl, supabaseKey);
