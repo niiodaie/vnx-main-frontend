a3a2ec4724fefa91153bec728a092e2792d53c63
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-22 03:04:47 -0500

    Update supabase.js

--- vnx-shell/tools/vnx-notebook/src/supabase.js
+++ vnx-shell/tools/vnx-notebook/src/supabase.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xuptevcpwsuxckuhsnwr.supabase.co'
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9;
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
 export const supabase = createClient(supabaseUrl, supabaseKey)
