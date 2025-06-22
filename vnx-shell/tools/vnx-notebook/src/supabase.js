9d4139bc0b36a16061428bb26ffd8082a0c77d68
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-22 02:42:08 -0500

    Update supabase.js

--- vnx-shell/tools/vnx-notebook/src/supabase.js
+++ vnx-shell/tools/vnx-notebook/src/supabase.js
@@ -1,7 +1,5 @@
 // src/supabase.js
-import React, { useState, useEffect } from 'react';
-import { supabase } from './supabase';
-import './App.css';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xuptevcpwsuxckuhsnwr.supabase.co'
 const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cHRldmNwd3N1eGNrdWhzbndyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NTgxNjksImV4cCI6MjA2NjEzNDE2OX0.eVNn5_PlNDOYD59OQf0EW0rs6X0QRPy5aQwnE7MGaF8
