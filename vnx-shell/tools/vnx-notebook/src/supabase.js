f5c574a0d2549e896e15988022f6728e1a1178f8
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-23 02:32:06 -0500

    Add files via upload

--- vnx-shell/tools/vnx-notebook/src/supabase.js
+++ vnx-shell/tools/vnx-notebook/src/supabase.js
@@ -1,6 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://xuptevcpwsuxckuhsnwr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cHRldmNwd3N1eGNrdWhzbndyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDU1ODE2OSwiZXhwIjoyMDY2MTM0MTY5fQ.xUPuBYzsqUFlXO8hKjjMRiBsATe3TcV05aZ0D1o6kNk';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// supabase.js - placeholder content
