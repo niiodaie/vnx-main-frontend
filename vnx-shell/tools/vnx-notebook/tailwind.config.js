131fb4f18cfb77d2d4b3aadf5e8121cfeeb3e9ff
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-23 04:45:34 -0500

    Update tailwind.config.js

--- vnx-shell/tools/vnx-notebook/tailwind.config.js
+++ vnx-shell/tools/vnx-notebook/tailwind.config.js
@@ -1,6 +1,9 @@
-
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
